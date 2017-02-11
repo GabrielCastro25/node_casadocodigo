285ba61be7ecc31aebfbcc9581474e981bc63ac4
GabrielCastro25 <gecastro66@gmail.com>
2017-02-11 12:47:25 -0200

    Development continue

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,14 +1,33 @@
 
 module.exports = function(app){
-    app.get('/produtos', function(req,res){
+	var listaProdutos = function(req,res){
     	var connection = app.infra.connectionFactory();
-    	var produtosBanco = app.infra.produtosBanco(connection);
+    	var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-
-    	produtosBanco.lista(function(err,results){
+    	produtosDAO.lista(function(err,results){
             res.render('produtos/lista', {lista:results});
         });
 
         connection.end();
+    }
+
+    app.get('/produtos', listaProdutos);
+
+    app.get('/produtos/form', function(req,res){
+    	res.render('produtos/form');
+    });
+
+    app.post('/produtos', function(req,res){
+    	var produto = req.body;
+    	
+    	var connection = app.infra.connectionFactory();
+    	var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+    	produtosDAO.salva(produto, function(err, results) {
+    		res.redirect('/produtos');
+    	});
+
+    	connection.end();
+
     });
 }
