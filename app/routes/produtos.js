4b99ab943853dd5cd9240f89da6099ccd25b4ae2
GabrielCastro25 <gecastro66@gmail.com>
2017-02-12 20:47:57 -0200

    Adiciona validação de erro

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -5,7 +5,15 @@ module.exports = function(app){
     	var produtosDAO = new app.infra.ProdutosDAO(connection);
 
     	produtosDAO.lista(function(err,results){
-            res.render('produtos/lista', {lista:results});
+    		res.format({
+    			html: function(){
+                    res.render('produtos/lista', {lista:results});
+                },
+                json: function(){
+                    res.json(results);
+                }
+    		})
+            
         });
 
         connection.end();
@@ -14,12 +22,34 @@ module.exports = function(app){
     app.get('/produtos', listaProdutos);
 
     app.get('/produtos/form', function(req,res){
-    	res.render('produtos/form');
+    	res.render('produtos/form', {errosValidacao:{}, produto: {}});
     });
+
+
+    function validarCamposObrigatorios(produto, req, res) {
+    	req.assert('titulo','Título é obrigatório').notEmpty();
+
+		req.assert('preco','Formato inválido').isFloat();
+
+		var erros = req.validationErrors();
+		if (erros){
+    		res.format({
+                html: function(){
+                    res.status(400).render("produtos/form",{validationErrors:errors,produto:produto});
+                },
+                json: function(){
+                    res.status(400).send(errors);
+                }
+            });
+   			return;
+		}
+    }
 
     app.post('/produtos', function(req,res){
     	var produto = req.body;
-    	
+
+    	validarCamposObrigatorios(produto, req, res);
+
     	var connection = app.infra.connectionFactory();
     	var produtosDAO = new app.infra.ProdutosDAO(connection);
 
